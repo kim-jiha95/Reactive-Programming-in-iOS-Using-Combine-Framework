513dd30186f33173df2c9479db2d0edc226d67c0
김지하 <kimjiha1112@gmail.com>
2024-08-02 17:29:49 +0900

    Initial Commit

---
+++ combine_ReactiveProgramming/combine_ReactiveProgrammingApp.swift
@@ -0,0 +1,17 @@
+//
+//  combine_ReactiveProgrammingApp.swift
+//  combine_ReactiveProgramming
+//
+//  Created by Jihaha kim on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct combine_ReactiveProgrammingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ combine_ReactiveProgrammingUITests/combine_ReactiveProgrammingUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  combine_ReactiveProgrammingUITestsLaunchTests.swift
+//  combine_ReactiveProgrammingUITests
+//
+//  Created by Jihaha kim on 8/2/24.
+//
+
+import XCTest
+
+final class combine_ReactiveProgrammingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
