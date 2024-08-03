2f685412c4e9460208ad1e66b0258fb679c89e3c
김지하 <kimjiha1112@gmail.com>
2024-08-03 22:45:36 +0900

    Error handling combine

--- combine_ReactiveProgramming/ContentView.swift
+++ combine_ReactiveProgramming/ContentView.swift
@@ -8,6 +8,34 @@
 import SwiftUI
 import Combine
 
+enum NumberError: Error {
+    case operationFailed
+}
+
+let numbersPublisher = [1, 2, 3, 4, 5].publisher
+
+let doubledPublisher = numbersPublisher
+    .tryMap { number in
+        if number == 4 {
+            throw NumberError.operationFailed
+        }
+        
+        return number * 2
+        
+    }.mapError { error in
+        return NumberError.operationFailed
+    }
+
+let cancellable = doubledPublisher.sink { completion in
+    switch completion {
+        case .finished:
+            print("finished")
+        case .failure(let error):
+            print(error)
+    }
+} receiveValue: { value in
+    print(value)
+}
 class ContentViewModel: ObservableObject {
     
     @Published var value: Int = 0
@@ -32,8 +60,8 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Text("\(vm.value)")
-                .font(.largeTitle)
+//            Text("\(vm.value)")
+//                .font(.largeTitle)
         }
         .padding()
     }
